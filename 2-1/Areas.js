f88478adc20c3b0a85406e7fc9635d8ad1bdc4ee
andole87 <andole87@gmail.com>
2019-04-07 22:11:59 +0900

    Create logic that check if args is number each functions.
    Each args must be Numbers. Add loop in each functions.
    When 1 args is not number at least, throws Error.

--- 2-1/Areas.js
+++ 2-1/Areas.js
@@ -2,6 +2,9 @@ function calculateCircleArea(r){
     if (arguments.length != 1) {
         throw "인자 개수가 부족합니다.";
     }
+    if (typeof arguments[i] != Number){
+        throw "숫자를 넣으세요.";
+    }
     const PI = 3.14;
     return r**2*PI;
 }
@@ -10,6 +13,11 @@ function calculateSquareArea() {
     const argsNumber = [1,2];
     if (!argsNumber.includes(arguments.length)){
         throw "인자의 개수가 안맞아여";
+    }
+    for (let i = 0; i < arguments.length; i++){
+        if (typeof arguments[i] != Number){
+            throw "숫자 넣어여";
+        }
     }
 
     switch (arguments.length){
@@ -24,6 +32,11 @@ function calculateTrapezoidArea(bottom, top, height) {
     if (arguments.length != 3) {
         throw "밑변, 윗변, 높이를 순서대로 넣으십셔";
     }
+    for (let i = 0; i < 3; i++){
+        if (typeof arguments[i] != Number){
+            throw "숫자 넣으셈";
+        }
+    }
 
     return (bottom + top) * height / 2;
 }
@@ -31,6 +44,11 @@ function calculateTrapezoidArea(bottom, top, height) {
 function calculateCylinderArea(radius, height) {
     if (arguments.length != 2) {
         throw "좋은 말로 할때 반지름, 높이를 순서대로 넣어라.";
+    }
+    for (let i = 0; i < 2; i++){
+        if (typeof arguments[i] != Number){
+            throw "숫자 느라고 XX";
+        }
     }
 
     return calculateCircleArea(radius) * height;
